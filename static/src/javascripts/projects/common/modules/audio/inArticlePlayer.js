80fb21d6f7133ce905e4eab17f14334f9c611d7e
Anna Leach <anna,leach@theguardian.com>
2018-08-31 15:24:25 +0100

    get player to work

--- static/src/javascripts/projects/common/modules/audio/inArticlePlayer.js
+++ static/src/javascripts/projects/common/modules/audio/inArticlePlayer.js
@@ -1,38 +1,124 @@
-import { sendToOphan } from './utils';
+// @flow
+import pauseBtn from 'svgs/journalism/in-article-audio-player/btn-pause.svg';
+import playBtn from 'svgs/journalism/in-article-audio-player/btn-play.svg';
+import { sendToOphan, formatTime } from './utils';
 
-const init = () => {
-    const inArticlePlayer = document.querySelector('.in-article-audio');
-    const mediaId = inArticlePlayer.getAttribute('data-media-id');
-
-    monitorPlay(inArticlePlayer, mediaId);
-
-    monitorPercentPlayed(inArticlePlayer, 25, mediaId);
-    monitorPercentPlayed(inArticlePlayer, 50, mediaId);
-    monitorPercentPlayed(inArticlePlayer, 75, mediaId);
-    monitorPercentPlayed(inArticlePlayer, 99, mediaId);
-}
-
-
-const monitorPlay = (inArticlePlayer, id) => {
-    inArticlePlayer.addEventListener('play', () => sendToOphan(id, 'play'));
-}
-
-const monitorPercentPlayed = (inArticlePlayer, marker, id) => {
-
-    let eventName = marker === 99 ? 'end' : marker.toLocaleString();
-
-    inArticlePlayer.addEventListener(
-        'timeupdate',
-        function listener(e){
-            let percentPlayed = Math.round(
-                (e.target.currentTime / e.target.duration) * 100
-            );
-            if(percentPlayed > marker) {
-                sendToOphan(id, eventName);
-                e.target.removeEventListener(e.type, listener)
-            }
-        }
-    )
+const updateProgressBar = percentPlayed => {
+    const progressBar: ?HTMLElement = document.querySelector(
+        '.inline-audio_content_progress-bar div.played'
+    );
+    if (progressBar) {
+        progressBar.style.width = `${percentPlayed}%`;
+    }
 };
 
-export {init};
+const updateTime = (el: HTMLElement, player) => {
+    player.addEventListener('timeupdate', () => {
+        el.textContent = formatTime(player.currentTime);
+        const percentPlayed = (player.currentTime / player.duration) * 100;
+        updateProgressBar(percentPlayed);
+    });
+};
+
+const setPlayButton = el => {
+    el.innerHTML = `<span>${playBtn.markup}</span>`;
+};
+
+const setPauseButton = el => {
+    el.innerHTML = `<span>${pauseBtn.markup}</span>`;
+};
+
+const addAudioControls = (el, player, id) => {
+    el.addEventListener('click', () => {
+        if (player.paused) {
+            sendToOphan(id, 'play');
+            player.play();
+            setPauseButton(el);
+        } else {
+            player.pause();
+            setPlayButton(el);
+        }
+    });
+};
+
+const activateScrubber = (el, player) => {
+    const w = el.offsetWidth;
+    const d = player.duration;
+    el.addEventListener('click', (e: MouseEvent | Touch) => {
+        const leftOffset = el.getBoundingClientRect().left;
+        const clickX = e.clientX - leftOffset;
+        const ratio = clickX / w;
+        console.log('w', w, 'clickx', clickX, 'ratio', ratio);
+        const selectedTime = d * ratio;
+        player.currentTime = selectedTime;
+        updateProgressBar(ratio * 100);
+    });
+};
+
+const monitorPercentPlayed = (player, marker, id) => {
+    const eventName = marker === 99 ? 'end' : marker.toLocaleString();
+
+    player.addEventListener('timeupdate', function listener(e) {
+        const percentPlayed = Math.round(
+            (player.currentTime / player.duration) * 100
+        );
+        if (percentPlayed > marker) {
+            sendToOphan(id, eventName);
+            player.removeEventListener(e.type, listener);
+        }
+    });
+};
+
+const playerObserved = (el, id) => {
+    const observer = new window.IntersectionObserver(
+        (entries, self) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    sendToOphan(id, 'ready');
+                    self.disconnect();
+                }
+            });
+        },
+        {
+            threshold: 1.0,
+        }
+    );
+    observer.observe(el);
+};
+
+const init = () => {
+    const player = document.querySelector('audio.inline-audio-player-element');
+
+    if (player && !(player instanceof HTMLMediaElement))
+        throw new Error("Expected an 'audio' element.");
+
+    const container: ?HTMLElement = document.querySelector(
+        '.inline-audio_container'
+    );
+
+    const mediaId: ?string =
+        player && player.hasAttribute('data-media-id')
+            ? player.getAttribute('data-media-id')
+            : '';
+    const buttonDiv: ?HTMLElement = document.querySelector(
+        '.inline-audio_button'
+    );
+    const scrubberBar: ?HTMLElement = document.querySelector(
+        '.inline-audio_content_progress-bar'
+    );
+    const timePlayedSpan = document.querySelector('#inline-audio_time-played');
+
+    if (player && mediaId && buttonDiv && scrubberBar && timePlayedSpan) {
+        playerObserved(container, mediaId);
+        addAudioControls(buttonDiv, player, mediaId);
+        updateTime(timePlayedSpan, player);
+        activateScrubber(scrubberBar, player);
+
+        monitorPercentPlayed(player, 25, mediaId);
+        monitorPercentPlayed(player, 50, mediaId);
+        monitorPercentPlayed(player, 75, mediaId);
+        monitorPercentPlayed(player, 99, mediaId);
+    }
+};
+
+export { init };
